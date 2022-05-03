588238f20b55fafed64b377d843e1e51b7bb84a5
efreham1 <hammarberg83@gmail.com>
2022-05-03 11:29:40 +0200

    Adjusted size to 1080p

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry('1100x800')
+window.geometry('1000x700')
 window.resizable(width=False, height=False)
-controls = tk.Frame(window, width=300, height=800)
-box = tk.Canvas(window, width=800, height=800, bg='white')
+controls = tk.Frame(window, width=250, height=700)
+box = tk.Canvas(window, width=750, height=700, bg='white')
 button_start = tk.Button(controls, text='Start!')
 button_stop = tk.Button(controls, text='Abort!')
 button_start.grid(row=0, column=0, sticky='ew')
