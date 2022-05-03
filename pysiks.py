d6b0e899febae2f49d575fef92f8ac66561c57e8
efreham1 <hammarberg83@gmail.com>
2022-05-03 12:47:19 +0200

    Started revamping of physics engine

--- pysiks.py
+++ pysiks.py
@@ -1,49 +1,29 @@
 import numpy as np
 
-class Object:
+class Physics_Canvas:
+    
+    def __init__(self, w, h, walls) -> None:
+        self.width = w
+        self.height = h
+        self.matrix = self.matrix_generator(walls) #(((w0, w1), (w2, w3)...), ((h0, h1)...), ((w0, w1)...), ((h0, h1)...)) starts at top goes clockwise
 
-    def __init__(self, m, x, y, vx, vy) -> None:
-        self.pos = np.array([float(x), float(y)])
-        self.vel = np.array([float(vx), float(vy)])
-        self.acc = np.zeros(2)
-        self.mass = m
-    
-    def update(self, dt, *forces):
-        F = np.zeros(2)
-        for f in forces:
-            F += f.get()
-        self.acc = F/self.mass
-        self.pos += self.vel*float(dt) + self.acc*float(dt)**2/2
-        self.vel += self.acc*float(dt)
-        
-    
+    def matrix_generator(self, walls):
+        matrix = np.zeros((self.height+2, self.width+2))
+        slicer = {
+            0 : matrix[0],
+            1 : matrix[:, -1],
+            2 : matrix[-1],
+            3 : matrix[:, 0]
+        }
+        for i, e in enumerate(walls):
+            print(e)
+            for ee in e:
+                print(ee)
+                if ee != ():
+                    for n in range(ee[0]+1, ee[1]+1):
+                        slicer[i][n] = 1
+        print(matrix)
 
-    def move_to(self, x, y):
-        self.pos = np.array([float(x), float(y)])
-    
-    def get_pos(self):
-        return self.pos
-    
-    def get_vel(self):
-        return self.vel
-    
-    def get_acc(self):
-        return self.acc
+e = Physics_Canvas(5, 5, (((0,5)), ((0,1), (3,5)), (), ()))
 
-class Force:
 
-    def __init__(self, x, y, t = 0) -> None:
-        self.F = np.array([float(x), float(y)])
-        self.time = float(t)
-
-    def update_t(self, dt):
-        self.time -= float(dt)
-        if self.time < 0:
-            self.F = np.zeros(2)
-    
-    def set_F(self, x, y):
-        self.F = np.array([float(x), float(y)])
-    
-    def get(self):
-        return self.F
-        
